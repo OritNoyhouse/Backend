3d6b74a40dbfdb66ec486c5ac21fe397675f206b
Orit Noyhouse <96670281+oritWolfson@users.noreply.github.com>
2022-10-30 17:52:25 +0200

    Update OrderIshpuzController.cs

--- HomeHospital/Controllers/OrderIshpuzController.cs
+++ HomeHospital/Controllers/OrderIshpuzController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -6,29 +6,19 @@ using System.Net.Http;
 using System.Web.Http;
 using Dto;
 using Bll;
+using System.Web.Http.Cors;
 
 namespace HomeHospital.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class OrderIshpuzController : ApiController
     {
-        // GET: api/OrderIshpuz
+        
         ClassDb classDb = new ClassDb();
-        public RequestResult Get()
-        {
-            return classDb.GetAllOrderHomeHospitalization();
-        }
+        
+       
 
-        // GET: api/OrderIshpuz/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/OrderIshpuz
-        public void Post(OrderHomeHospitalizationDto a)
-        {
-            classDb.AddOrderHomeHospitalization(a);
-        }
+       
 
         // PUT: api/OrderIshpuz/5
         public void Put(int id, [FromBody]string value)
@@ -39,6 +29,19 @@ namespace HomeHospital.Controllers
         public void Delete(OrderHomeHospitalizationDto a)
         {
             classDb.RemovOrderHomeHospitalization(a);
+        }
+        [HttpGet]
+        [Route("api/OrderIshpuz/9/{id}")]
+        public object GettOrders(int id)
+        {
+
+            return classDb.GetAllOrderHomeHospitalization(id).Data; 
+        }
+        [HttpPost]
+        [Route("api/OrderIshpuz/18")]
+        public void PostOrderHomeHospitalization(OrderHomeHospitalizationDto a)
+        {
+            classDb.AddOrderHomeHospitalization(a);
         }
     }
 }

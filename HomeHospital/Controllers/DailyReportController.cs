087049ce7eb2ad9f0cc16135695b90c97acc04c6
Orit Noyhouse <96670281+oritWolfson@users.noreply.github.com>
2022-10-30 17:47:54 +0200

    Update DailyReportController.cs

--- HomeHospital/Controllers/DailyReportController.cs
+++ HomeHospital/Controllers/DailyReportController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -6,39 +6,33 @@ using System.Net.Http;
 using System.Web.Http;
 using Dto;
 using Bll;
-
+using System.Web.Http.Cors;
 namespace HomeHospital.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class DailyReportController : ApiController
     {
         ClassDb classDb = new ClassDb();
-        // GET: api/DailyReport
-        public RequestResult Get()
-        {
-            return classDb.GetAllDailyReportToDoctor();
-        }
-
-        // GET: api/DailyReport/5
-        public string Get(int id)
-        {
-            return "value";
-        }
 
         // POST: api/DailyReport
-        public void Post(DailyReportToDoctorDto a)
+        public void Post(DailyReportToDoctorDto dailyReport)
         {
-            classDb.AddDailyReportToDoctor(a);
-        }
-
-        // PUT: api/DailyReport/5
-        public void Put(int id, [FromBody]string value)
-        {
+            classDb.AddDailyReportToDoctor(dailyReport);
         }
 
         // DELETE: api/DailyReport/5
-        public void Delete( DailyReportToDoctorDto a)
+        public void Delete( DailyReportToDoctorDto dailyReport)
         {
-            classDb.RemovDailyReportToDoctor(a);
+            classDb.RemovDailyReportToDoctor(dailyReport);
         }
+        [HttpGet]
+        [Route("api/DailyReport/9/{id}/{date}")]
+        public object GettDailyReport(int id,int date) 
+        {
+            object obj = classDb.GetDailyReportToDoctor(id,date).Data;
+            return obj;
+        }
+
     }
 }
+

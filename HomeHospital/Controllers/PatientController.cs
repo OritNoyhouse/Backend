41004ed59ee7f539617e612a0448203def7c6bce
Orit Noyhouse <96670281+oritWolfson@users.noreply.github.com>
2022-10-30 17:52:54 +0200

    Update PatientController.cs

--- HomeHospital/Controllers/PatientController.cs
+++ HomeHospital/Controllers/PatientController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -6,30 +6,20 @@ using System.Net.Http;
 using System.Web.Http;
 using Dto;
 using Bll;
+using System.Web.Http.Cors;
 
 namespace HomeHospital.Controllers
 {
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class PatientController : ApiController
     {
+       
         ClassDb classDb = new ClassDb();
-        // GET: api/Patient
-        public RequestResult Get()
-        {
-            return classDb.GetAllPatients();
-        }
+       
 
-        // GET: api/Patient/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Patient
-        public void Post(PatientsDto a)
-        {
-            classDb.AddPatients(a);
-        }
-
+       
+       
         // PUT: api/Patient/5
         public void Put(int id, [FromBody]string value)
         {
@@ -40,5 +30,21 @@ namespace HomeHospital.Controllers
         {
             classDb.RemovePatients(a);
         }
+       
+       
+        [HttpPost]
+        [Route("api/Patient/8")]
+        public void PostPatient(PatientsDto a)
+        {
+             classDb.AddPatients(a);
+        }
+        [HttpGet]
+        [Route("api/Patient/18/{password}")]
+        public object GettPatient( string password)
+        {
+            
+            return classDb.GetPatient(password).Data; ;
+        }
+       
     }
 }

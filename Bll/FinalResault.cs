29cf2a17b7cb1821f0aed8953b3231de62adc3cf
Orit Noyhouse <96670281+oritWolfson@users.noreply.github.com>
2022-10-30 17:58:38 +0200

    Update and rename Class1.cs to FinalResault.cs

---
+++ Bll/FinalResault.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bll
+{
+  public  class FinalResault
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public string street { get; set; }
+        public int numOfBulding { get; set; }
+        public string city { get; set; }
+    }
+}

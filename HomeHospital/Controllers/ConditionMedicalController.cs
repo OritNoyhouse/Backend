bdf35612a0e703a0f1ab961358f642eb0219ac4c
Orit Noyhouse <96670281+oritWolfson@users.noreply.github.com>
2022-10-30 17:47:24 +0200

    Update ConditionMedicalController.cs

--- HomeHospital/Controllers/ConditionMedicalController.cs
+++ HomeHospital/Controllers/ConditionMedicalController.cs
@@ -16,29 +16,19 @@ namespace HomeHospital.Controllers
         public RequestResult Get()
         {
             return classDb.GetAllConditationMedical();
-        }
-
-        // GET: api/ConditionMedical/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+        }       
 
         // POST: api/ConditionMedical
-        public void Post(ConditationMedicalDto a)
+        public void Post(ConditationMedicalDto conditationMedical)
         {
-            classDb.AddConditationMedical(a);
+            classDb.AddConditationMedical(conditationMedical);
         }
 
-        // PUT: api/ConditionMedical/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
 
         // DELETE: api/ConditionMedical/5
-        public void Delete(ConditationMedicalDto a)
+        public void Delete(ConditationMedicalDto conditationMedical)
         {
-            classDb.RemovConditationMedical(a);
+            classDb.RemovConditationMedical(conditationMedical);
         }
     }
 }
